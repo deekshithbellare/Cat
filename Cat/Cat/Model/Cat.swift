8780bca0e8ec06a172a1713b91b41e42d7fe1898
deekshithbellare <ideekshi@icloud.com>
2020-08-04 21:30:02 +0530

    Cat Model added

---
+++ Cat/Cat/Model/Cat.swift
@@ -0,0 +1,16 @@
+//
+//  Cat.swift
+//  Cat
+//
+//  Created by Deekshith Bellare on 04/08/20.
+//  Copyright © 2020 Deekshith Bellare. All rights reserved.
+//
+
+import Foundation
+// Cat Model
+struct Cat:Codable {
+    //Unique identfier for the cat
+    var id:String
+    //Url for the image of cat
+    var url:String
+}
---
+++ Cat/Cat/Scenes/CatListing/CatsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CatsViewModel.swift
+//  Cat
+//
+//  Created by Deekshith Bellare on 04/08/20.
+//  Copyright © 2020 Deekshith Bellare. All rights reserved.
+//
+
+import Foundation
