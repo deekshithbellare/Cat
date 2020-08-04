507d1d7abbad06ff62e6a3d1ee2eabb6558b62b1
deekshithbellare <ideekshi@icloud.com>
2020-08-05 00:35:32 +0530

    UIView Extenstion: BindToSuperView

---
+++ Cat/Cat/Extenstions/UIView.swift
@@ -0,0 +1,28 @@
+//
+//  UIView.swift
+//  Cat
+//
+//  Created by Deekshith Bellare on 05/08/20.
+//  Copyright © 2020 Deekshith Bellare. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    /// Adds constraints to this `UIView` instances `superview` object to make sure this always has the same size as the superview.
+    /// Please note that this has no effect if its `superview` is `nil` – add this `UIView` instance as a subview before calling this.
+    func bindToSuperviewBounds() {
+        guard let superview = self.superview else {
+            print("Error! `superview` was nil – call `addSubview(view: UIView)` before calling `bindFrameToSuperviewBounds()` to fix this.")
+            return
+        }
+
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.topAnchor.constraint(equalTo: superview.topAnchor, constant: 0).isActive = true
+        self.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: 0).isActive = true
+        self.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: 0).isActive = true
+        self.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: 0).isActive = true
+
+    }
+}
